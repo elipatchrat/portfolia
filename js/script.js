180fb133722898b1612d41c58149c486138c76ec
elipatchrat <elipatchrat@gmail.com>
2026-03-21 20:01:53 +1100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ const projectData = {
         title: "Real-Time Chat Interface",
         date: "Oct 2025",
         description: "A simple chat application utilizing WebSockets (Socket.IO) for instant, bi-directional communication, focusing on minimal latency.",
-        link: "#",
+        link: "https://realtimechat-yxyl.onrender.com",
         linkText: "View Code"
     },
     "weather-api": {
